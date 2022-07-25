d5d713014bb52909a3fa70433e55e6d07b7a0ea2
shilujun <77585574@qq.com>
2022-07-25 20:06:38 +0800

    ssl Exception 处理

--- app/src/main/java/com/cashhub/cash/app/WebActivity.java
+++ app/src/main/java/com/cashhub/cash/app/WebActivity.java
@@ -87,25 +87,25 @@ public class WebActivity extends BaseActivity implements View.OnClickListener {
         return null;
       }
 
-      @Override
-      public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
-        builder.setMessage(R.string.notification_error_ssl_cert_invalid);
-        builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
-          @Override
-          public void onClick(DialogInterface dialog, int which) {
-            handler.proceed();
-          }
-        });
-        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-          @Override
-          public void onClick(DialogInterface dialog, int which) {
-            handler.cancel();
-          }
-        });
-        final AlertDialog dialog = builder.create();
-        dialog.show();
-      }
+//      @Override
+//      public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+//        final AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+//        builder.setMessage(R.string.notification_error_ssl_cert_invalid);
+//        builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
+//          @Override
+//          public void onClick(DialogInterface dialog, int which) {
+//            handler.proceed();
+//          }
+//        });
+//        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+//          @Override
+//          public void onClick(DialogInterface dialog, int which) {
+//            handler.cancel();
+//          }
+//        });
+//        final AlertDialog dialog = builder.create();
+//        dialog.show();
+//      }
     });
 
     WebSettings webSettings = mWebView.getSettings();
--- app/src/main/java/com/cashhub/cash/app/WebviewActivity.java
+++ app/src/main/java/com/cashhub/cash/app/WebviewActivity.java
@@ -84,25 +84,25 @@ public class WebviewActivity extends BaseActivity {
         return null;
       }
 
-      @Override
-      public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
-        builder.setMessage(R.string.notification_error_ssl_cert_invalid);
-        builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
-          @Override
-          public void onClick(DialogInterface dialog, int which) {
-            handler.proceed();
-          }
-        });
-        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-          @Override
-          public void onClick(DialogInterface dialog, int which) {
-            handler.cancel();
-          }
-        });
-        final AlertDialog dialog = builder.create();
-        dialog.show();
-      }
+//      @Override
+//      public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+//        final AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+//        builder.setMessage(R.string.notification_error_ssl_cert_invalid);
+//        builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
+//          @Override
+//          public void onClick(DialogInterface dialog, int which) {
+//            handler.proceed();
+//          }
+//        });
+//        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+//          @Override
+//          public void onClick(DialogInterface dialog, int which) {
+//            handler.cancel();
+//          }
+//        });
+//        final AlertDialog dialog = builder.create();
+//        dialog.show();
+//      }
     });
 
     //防止遇到重定向
